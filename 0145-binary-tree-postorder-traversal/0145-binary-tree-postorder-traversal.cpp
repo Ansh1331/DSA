de5384b8f9023e31803d1d4d718d12be5750ab0d
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-01-16 18:43:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (85.51%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,6 +11,62 @@
  */
 class Solution {
 public:
+    //USING 1 STACK 
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack <TreeNode*> st;
+        TreeNode* curr = root;
+        while(curr || !st.empty())
+        {
+            while(curr)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }
+            TreeNode *temp = st.top()->right;
+            if(temp!=NULL) curr = temp;
+            else
+            {
+                temp = st.top();
+                ans.push_back(temp->val);
+                st.pop();
+                while(!st.empty()&&st.top()->right==temp)
+                {
+                    temp = st.top();
+                    ans.push_back(st.top()->val);
+                    st.pop();
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+   
+    
+
+    /*
+    SIMPLE
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        postorder(root,res);
+        return res;
+    }
+
+    void postorder(TreeNode* root,vector<int>& res){
+        if(root==NULL) return;
+
+        postorder(root->left,res);
+        postorder(root->right,res);
+        res.push_back(root->val);
+    }   
+    */
+
+
+    /*
+    USING 2 STACKS
+
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
         stack<TreeNode*> st1,st2;
@@ -33,23 +89,5 @@ public:
         }
         return res;
     }
-};
 
-   
-    
-
-    /*
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> res;
-        postorder(root,res);
-        return res;
-    }
-
-    void postorder(TreeNode* root,vector<int>& res){
-        if(root==NULL) return;
-
-        postorder(root->left,res);
-        postorder(root->right,res);
-        res.push_back(root->val);
-    }   
     */
