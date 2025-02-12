de769104a7b777b50185494b948092efc0341663
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-02-12 16:45:06 +0530

    Time: 0 ms (100%), Space: 53 MB (65.56%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,42 @@
 class Solution {
-public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
+private:
+    bool dfs(vector<vector<int>>& graph,vector<int>& vis,vector<int>& pathvis,int node,vector<int>& check){
+        vis[node]=1;
+        pathvis[node]=1;
+
+        for(auto it: graph[node]){
+            if(!vis[it]){
+                if(dfs(graph,vis,pathvis,it,check)) return true;
+            }
+            else if(vis[it] && pathvis[it]){
+                return true;
             }
         }
+        check[node]=1;
+        pathvis[node]=0;
+        return false;
     }
-    
+public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
-        queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+        int V=graph.size();
+        vector<int> vis(V,0);
+        vector<int> pathvis(V,0);
+        vector<int> check(V,0);
+        vector<int> safenodes;
+
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(graph,vis,pathvis,i,check);
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+
+        for(int i=0;i<V;i++){
+            if(check[i]==1){
+                safenodes.push_back(i);
+            }
+        }
+
+        return safenodes;
+
     }
 };
