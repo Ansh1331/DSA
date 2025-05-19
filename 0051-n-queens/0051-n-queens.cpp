eebcdaf9b4ce7ad036b7b1e9907999a77bfaa2f2
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-20 00:43:54 +0530

    Time: 2 ms (75.74%), Space: 10.1 MB (74.96%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,65 @@
 class Solution {
 public:
+    bool isSafe(int row,int col,vector<string>&board,int n){
+        int drow=row;
+        int dcol=col;
 
-    void solve(int col,int n,vector<vector<string>> &res, vector<string> &board, vector<int> & lowerdiagnol,vector<int> & leftrow,vector<int> & upperdiagnol){
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row--;
+            col--;
+        }
+
+        row=drow;
+        col=dcol;
+        while(col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            col--;
+        }
+
+        row=drow;
+        col=dcol;
+        while(col>=0 && row<n){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+
+    void helper(int col,vector<vector<string>>& res,vector<string>&board,int n){
         if(col==n){
             res.push_back(board);
             return;
         }
 
         for(int row=0;row<n;row++){
-            if(lowerdiagnol[row+col]==0 && leftrow[row]==0 && upperdiagnol[n-1 + row-col]==0){
+            if(isSafe(row,col,board,n)){
                 board[row][col]='Q';
-                leftrow[row]=1;
-                lowerdiagnol[row+col]=1;
-                upperdiagnol[n-1+ row-col]=1;
-                solve(col+1,n, res, board, lowerdiagnol, leftrow ,upperdiagnol);
+                helper(col+1,res,board,n);
                 board[row][col]='.';
-                leftrow[row]=0;
-                lowerdiagnol[row+col]=0;
-                upperdiagnol[n-1+ row-col]=0;
             }
         }
     }
+
 
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
         vector<string> board(n);
         string s(n,'.');
-
         for(int i=0;i<n;i++){
             board[i]=s;
         }
+        int col=0;
 
-        vector<int> lowerdiagnol(2*n-1, 0), leftrow(n,0), upperdiagnol(2*n-1, 0);
-        solve(0,n, res, board, lowerdiagnol, leftrow ,upperdiagnol);
+        helper(col,res,board,n);
         return res;
     }
-}; 
+};
