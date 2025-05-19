df385c37c7dd988b7501e379f72a82ab63c478e5
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-19 23:09:55 +0530

    Time: 790 ms (36.84%), Space: 11.8 MB (25.32%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool helper(vector<vector<char>>& board,string word, int n, int m, int index,int i,int j){
-        if(index== word.size()){
+    bool helper(int n,int m,int index,vector<vector<char>>&board,string word,int i,int j){
+        if(index==word.size()){
             return true;
         }
-        if(i<0 || j<0 || i==n || j==m || board[i][j]!= word[index]) return false;
+        if(i<0 || j<0 || i==n || j==m || board[i][j]!=word[index]) return false;
 
         char ch=board[i][j];
-        board[i][j]= '#'; //denote a letter is visited
+        board[i][j]='#';
 
-        bool op1= helper(board,word,n,m,index+1,i+1,j);
-        bool op2= helper(board,word,n,m,index+1,i,j+1);
-        bool op3= helper(board,word,n,m,index+1,i-1,j);
-        bool op4= helper(board,word,n,m,index+1,i,j-1);
+        bool op1=helper(n,m,index+1,board,word,i+1,j);
+        bool op2=helper(n,m,index+1,board,word,i,j+1);
+        bool op3=helper(n,m,index+1,board,word,i,j-1);
+        bool op4=helper(n,m,index+1,board,word,i-1,j);
 
         board[i][j]=ch;
-        return op1 || op2 || op3|| op4;
-    }
+        return op1||op2||op3||op4;
 
+    }
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size();
         int m=board[0].size();
@@ -25,13 +25,12 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    if(helper(board,word,n,m,index,i,j)){
+                    if(helper(n,m,index,board,word,i,j)){
                         return true;
-                    }
+                    };
                 }
             }
         }
         return false;
-
     }
 };
