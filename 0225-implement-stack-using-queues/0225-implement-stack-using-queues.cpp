2daefa583ce6377f2d9424c40d41441147f364a1
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-20 17:48:20 +0530

    Time: 0 ms (100%), Space: 9.5 MB (54.98%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,44 +1,42 @@
-#include <queue>
-using namespace std;
-
 class MyStack {
-private:
-    queue<int> queue1, queue2;
-
 public:
+    queue<int> q1;
+    queue<int> q2;
     MyStack() {}
-
+    // 1 2 3
+    // 3 2 1
     void push(int x) {
-        queue1.push(x);
+        q1.push(x);
     }
-
+    
     int pop() {
-        while (queue1.size() > 1) {
-            queue2.push(queue1.front());
-            queue1.pop();
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
         }
-        int topElement = queue1.front();
-        queue1.pop();
-        swap(queue1, queue2);
-        return topElement;
+        int el=q1.front();
+        q1.pop();
+        swap(q1,q2);
+        return el;
     }
-
+    
     int top() {
-        while (queue1.size() > 1) {
-            queue2.push(queue1.front());
-            queue1.pop();
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
         }
-        int topElement = queue1.front();
-        queue2.push(queue1.front());
-        queue1.pop();
-        swap(queue1, queue2);
-        return topElement;
+        int el=q1.front();
+        q2.push(q1.front());
+        q1.pop();
+        swap(q1,q2);
+        return el;
     }
-
+    
     bool empty() {
-        return queue1.empty();
+        return q1.empty();
     }
 };
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();
