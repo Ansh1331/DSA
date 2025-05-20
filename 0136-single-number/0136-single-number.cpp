cd302e13dcc8d41aae6f6b7ea7e96ab099523b78
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-20 17:22:09 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.86%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& arr) {
-        // int n=arr.size();
-        // sort(arr.begin(),arr.end());
-        // int res=-1;
-
-        // for(int i=0;i<n-1;i+=2){
-        //     if(arr[i] != arr[i+1]){
-        //         return arr[i];
-        //     }
-        // }
-        // return arr[n-1];
-        int n=arr.size();
-        int xorr=0;
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int res=0;
         for(int i=0;i<n;i++){
-            xorr= xorr ^ arr[i];
+            res ^= nums[i];
         }
-
-        return xorr;
+        return res;
     }
 };
