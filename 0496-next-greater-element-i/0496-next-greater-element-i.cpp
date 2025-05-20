092527ec46705f30af4a1d71b3bed7dd4ee619be
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-20 19:10:42 +0530

    Time: 0 ms (100%), Space: 12.8 MB (58.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,34 +1,23 @@
-#include<stack>
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums2.size();
-        vector<int> res(n,-1);
-        vector<int> final(nums1.size());
+        int n=nums1.size();
+        int m=nums2.size();
         stack<int> st;
+        vector<int> res(n);
+        unordered_map<int,int> greater;
 
-        for(int i=n-1;i>=0;i--){
-            while (!st.empty() && st.top() <= nums2[i]) {
+        for(int i=m-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
-            if (!st.empty()) {
-                res[i] = st.top();
-            }
+
+            greater[nums2[i]] = st.empty() ? -1:st.top();
             st.push(nums2[i]);
         }
-
-        for(int k=0;k<nums1.size();k++){
-            int index=-1;
-            for(int l=0;l<nums2.size();l++){
-                if(nums1[k]==nums2[l]){
-                    index=l;
-                    break;
-                }
-            }
-            if(index!=-1){
-                final[k]=res[index];
-            }
+        for(int i=0;i<n;i++){
+            res[i]=greater[nums1[i]];
         }
-        return final;
+        return res;
     }
 };
