3f01da05d468b0647e0ae4ffceeeb5cac00a28df
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-22 14:17:34 +0530

    Time: 108 ms (79.52%), Space: 224.1 MB (85.78%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int sz = queries.size();
-        sort(queries.begin(), queries.end());
-        vector<int> dif(n + 1);
-        int sum = 0;
-        int j = 0;
+        int n=nums.size();
+        int sz=queries.size();
+        int j=0;
+        int sum=0;
+        sort(queries.begin(),queries.end());
+        int count=0;
         priority_queue<int> pq;
-        int cnt = 0;
+        vector<int> diff(n+1,0);
 
-        for (int i = 0; i < n; i++) {
-            while (j < sz && queries[j][0] <= i) {
+        for(int i=0;i<n;i++){
+            while(j<sz && queries[j][0]<=i){
                 pq.push(queries[j][1]);
                 j++;
             }
 
-            while (sum + dif[i] < nums[i]) {
-                if (pq.empty()) {
-                    return -1;
-                }
+            while(sum+diff[i] < nums[i]){
+                if(pq.empty()){
+                return -1;}
+                    
 
-                int a = pq.top();
+                int a=pq.top();
                 pq.pop();
-                if (a >= i) {
-                    dif[i]++;
-                    dif[a + 1]--;
-                    cnt++;
+                if(a>=i){
+                    diff[i]++;
+                    diff[a+1]--;
+                    count++;
                 }
             }
-
-            sum += dif[i];
+            sum+=diff[i];
         }
-
-        return sz-cnt;
+        return sz-count;
     }
 };
