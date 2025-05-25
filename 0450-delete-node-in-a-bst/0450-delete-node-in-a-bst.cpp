0e6955b5008e49a502ccbe19cf37ebb3f6c96243
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-25 14:13:38 +0530

    Time: 0 ms (100%), Space: 34.2 MB (93.59%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,6 +11,22 @@
  */
 class Solution {
 public:
+    TreeNode* findrightmost(TreeNode* node){
+        while(node->right){
+            node=node->right;
+        }
+        return node;
+    }
+    TreeNode* helper(TreeNode* root){
+   //     TreeNode* rightmost= findright(root);
+        if(root->left==NULL) return root->right;
+        else if(root->right==NULL) return root->left;
+        TreeNode* right=root->right;
+        TreeNode* rightmostinleft= findrightmost(root->left);
+        rightmostinleft->right=right;
+        return root->left;
+    }
+    
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL) return root;
         if(root->val == key){
@@ -18,45 +34,25 @@ public:
         }
         TreeNode* dummy=root;
         while(root){
-            if(root->val > key){
-                if(root->left && root->left->val == key){
-                    root->left=helper(root->left);
-                    break;
-                }
-                else{
-                    root=root->left;
-                }
-            }
-            else{
-                if(root->right && root->right->val == key){
+            if(root->val < key){
+                if(root->right && root->right->val==key){
                     root->right=helper(root->right);
-                    break;
+                    break ;
                 }
                 else{
                     root=root->right;
                 }
             }
+            else{
+                if(root->left && root->left->val==key){
+                    root->left=helper(root->left);
+                    break ;
+                }
+                else{
+                    root=root->left;
+                }
+            }
         }
         return dummy;
-    }
-
-    TreeNode* helper(TreeNode* root){
-        if(root->left == NULL){
-            return root->right;
-        }
-        else if(root->right==NULL){
-            return root->left;
-        }
-        TreeNode* rightchild=root->right;
-        TreeNode* rightMostInLeftTree= findRightMost(root->left);
-        rightMostInLeftTree->right=rightchild;
-        return root->left;
-    }
-
-    TreeNode* findRightMost(TreeNode* root){
-        while(root->right){
-            root=root->right;
-        }
-        return root;
     }
 };
