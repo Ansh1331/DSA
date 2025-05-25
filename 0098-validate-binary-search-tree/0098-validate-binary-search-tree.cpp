3b2faf504e933ae8c902cdaabe2b6c0af43ad397
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-25 14:31:43 +0530

    Time: 0 ms (100%), Space: 21.9 MB (47.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,24 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        return helper(root, LONG_MIN, LONG_MAX);
+    void inorder(TreeNode* root, vector<int>& inorderTraversal) {
+        if (root == nullptr) return;
+        inorder(root->left, inorderTraversal);
+        inorderTraversal.push_back(root->val);
+        inorder(root->right, inorderTraversal);
     }
 
-    bool helper(TreeNode* root, long minVal, long maxVal){
-        if(root==NULL) return true;
-        if(root->val >= maxVal || root->val <= minVal) return false;
-        return helper(root->left,minVal,root->val) &&
-               helper(root->right,root->val,maxVal);
+    bool isValidBST(TreeNode* root) {
+        vector<int> inorderTraversal;
+        inorder(root, inorderTraversal);
+        for (int i = 1; i < inorderTraversal.size(); ++i) {
+            if (inorderTraversal[i] <= inorderTraversal[i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 };
