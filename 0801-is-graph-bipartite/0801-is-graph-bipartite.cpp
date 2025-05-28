bf83c9545ffc4b3bca1595b9a50b312c6afb998c
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-05-28 15:44:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (13.59%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,33 +1,30 @@
-#include <vector>
-#include <queue>
-using namespace std;
-
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, -1);  // Initialize all nodes as uncolored (-1)
+        int n=graph.size();
+        vector<int> color(n,-1);
 
-        for (int i = 0; i < n; i++) {  // Iterate over all components
-            if (color[i] == -1) {  // If node is uncolored, start BFS
-                queue<int> q;
-                q.push(i);
-                color[i] = 0;  // Start coloring with 0
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
 
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
+            queue<int> q;
+            q.push(i);
+            color[i]=0;
 
-                    for (auto it : graph[node]) {
-                        if (color[it] != -1 && color[it] == color[node]) {
-                            return false;  // Conflict found
-                        }
-                        else if (color[it] == -1) {  
-                            color[it] = !color[node];  // Assign opposite color
-                            q.push(it);
-                        }
+            while(!q.empty()){
+                int node=q.front();
+                q.pop();
+
+                for(auto it: graph[node]){
+                    if(color[it] != -1 && color[node]==color[it]){
+                        return false;
+                    }
+                    else if(color[it] ==-1){
+                        color[it]=!color[node];
+                        q.push(it);
                     }
                 }
+            }
             }
         }
         return true;
