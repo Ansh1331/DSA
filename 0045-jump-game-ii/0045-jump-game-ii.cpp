670a21391eac6f21f4e6cbae0b414b13e2e534ac
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-07-02 19:07:44 +0530

    Time: 0 ms (100%), Space: 20.5 MB (36.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& arr) {
-        int n=arr.size();
-        int l=0;
-        int r=0;
         int jumps=0;
-        
-        while(r<n-1){
-            int farthest=0;
+        int l=0,r=0;
+
+        while(r<arr.size()-1){
+            int maxJump=l;
+
             for(int i=l;i<=r;i++){
-                farthest=max(farthest,i+arr[i]);
+                maxJump= max(maxJump, i+arr[i]);
             }
             l=r+1;
-            r=farthest;
+            r=maxJump;
             jumps++;
         }
         return jumps;
