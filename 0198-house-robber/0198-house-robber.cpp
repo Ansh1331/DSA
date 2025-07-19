6239eb41082cc5c03435e46bfa5f0d6885459a0e
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-07-19 23:12:18 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -18,6 +18,19 @@ public:
     int rob(vector<int>& arr) {
         int n=arr.size();
         vector<int> dp(n+1,-1);
-        return helper(n-1,dp,arr);
+        // return helper(n-1,dp,arr);
+        dp[0]= arr[0];
+
+        for(int i=1;i<n;i++){
+            int pick= arr[i];
+            if(i>1){
+                pick= arr[i] + dp[i-2];
+            }
+            //skip
+            int skip=dp[i-1];
+            
+            dp[i]=max(pick,skip);
+        }
+        return dp[n-1];
     }
 };
