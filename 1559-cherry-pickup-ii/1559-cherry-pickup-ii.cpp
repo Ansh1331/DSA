6da90c5378b47558cfb7f435300927ea9c87262f
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-07-21 22:40:53 +0530

    Time: 47 ms (69.55%), Space: 20.9 MB (53.25%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int helper(int i,int j1, int j2,  vector<vector<vector<int>>>& dp,int n,int m,vector<vector<int>>& grid){
-        if(j1<0 || j1>m-1 || j2<0 || j2>m-1) return -1e8;
-        if(i==n-1) {
-            if(j1!=j2) return grid[i][j1] + grid[i][j2];
-            else return grid[i][j1];
+    int helper(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp,int n,int m){
+        if(j1<0 || j2<0 || j1>m-1 || j2>m-1){
+            return -1e8;
+        }   
+        if(i == n-1){
+            if(j1==j2){
+                return grid[i][j1];
+            }
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
         }
+
         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
         int maxi=-1e8;
-        for(int delr=-1 ; delr<=1; delr++){
-            for(int delc=-1;delc <= 1 ; delc++){
+        for(int delrow=-1; delrow<=1;delrow++){
+            for(int delcol=-1; delcol<=1;delcol++){
                 int value=0;
-                if(j1!=j2) value= grid[i][j1] + grid[i][j2];
-                else value= grid[i][j1];
-                value += helper(i+1, j1+delr, j2+delc, dp,n,m,grid);
+                if(j1!=j2){
+                value= grid[i][j1] + grid[i][j2];
+                }   
+                else value=grid[i][j1];
+                value+= helper(i+1,j1+delrow,j2+delcol,grid,dp,n,m);
                 maxi= max(maxi,value);
             }
         }
@@ -23,9 +32,8 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<int>>> dp (n,vector<vector<int>> (m,vector<int> (m,-1)));
-        return helper(0,0,m-1,dp,n,m,grid);
-        //0,0 robot-1, 0,m-1 robot-2
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int> (m,-1)));
 
+        return helper(0,0,m-1,grid,dp,n,m);
     }
 };
