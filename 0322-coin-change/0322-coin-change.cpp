8eb87b06abe13221bbb51f9d3392d00e5f804d23
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-07-31 19:33:45 +0530

    Time: 51 ms (25.89%), Space: 49.4 MB (30.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1)); 
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i] = i/coins[0];
-            }
-            else dp[0][i]=1e8;
+    int helper(int index,int amount, vector<int>& arr, vector<vector<int>>& dp){
+        if(amount==0) return 0;
+        if(index==0){
+            if(arr[0] > amount) return 1e8;
+            if(amount % arr[0] == 0 ) return amount/arr[0];
+            return 1e8;
         }
 
-        for(int index=1;index<n;index++){
-            for(int target=0;target<=amount;target++){
-                int nottake= dp[index-1][target];
-                int take=1e8;
-                if(coins[index] <= target) take=1+ dp[index][target-coins[index]];
-
-                dp[index][target]= min(nottake,take);
-            }
+        if(dp[index][amount] != -1) return dp[index][amount];
+        int nottake= helper(index-1,amount,arr,dp);
+        int take=1e8;
+        if(arr[index] <= amount){
+            take= 1+ helper(index,amount-arr[index],arr,dp);
         }
-        int ans=dp[n-1][amount];
-        return ans>=1e7 ? -1:ans;
 
+        return dp[index][amount] = min(nottake,take);
+    }
+
+    int coinChange(vector<int>& arr, int amount) {
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans= helper(n-1,amount,arr,dp);
+        return ans>=1e8? -1:ans;
     }
 };
