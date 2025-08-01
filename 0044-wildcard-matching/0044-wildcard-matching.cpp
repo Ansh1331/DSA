2be4797b2782229d21e56a1845075656a7a4ecfb
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-08-01 23:25:45 +0530

    Time: 41 ms (15.5%), Space: 31.4 MB (36.36%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     bool helper(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
+        if(j<0 && i<0) return true;
         if(j<0 && i>=0) return false;
-        if(i<0 && j>=0) {
-            for (int k = 0; k <= j; ++k) {
-                if (t[k] != '*') return false;
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+                if(t[k] != '*') return false;
             }
             return true;
         }
 
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j] || t[j]=='?') {
-            return dp[i][j]= helper(i-1,j-1,s,t,dp);
+
+        if(s[i] == t[j] || t[j]=='?'){
+            return dp[i][j]=helper(i-1,j-1,s,t,dp);
         }
-        else if(t[j] == '*'){
-            return dp[i][j]= helper(i-1,j,s,t,dp) || helper(i,j-1,s,t,dp);
+        else if(t[j]=='*'){
+            return dp[i][j]=helper(i-1,j,s,t,dp) || helper(i,j-1,s,t,dp);
         }
         else{
             return false;
         }
+
     }
 
     bool isMatch(string s, string t) {
