afca80f8166ea7446e63154cb19e4844260f365c
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-08-05 22:54:19 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.92%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        int index=-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid] == target){
+                index=mid;
+                return mid;
+            }
+            else if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target<=arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(arr[mid] <= target && target<=arr[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
