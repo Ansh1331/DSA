19b1b9f0fd40ed66418708a51a874e0c5d7ec640
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-08-30 00:51:47 +0530

    Time: 75 ms (66.68%), Space: 173 MB (88.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,66 +1,67 @@
-class Node{
-    public:     
-        int key;
-        int val;
-        Node* next;
-        Node* prev;
-        Node(int _key,int _val){
-            key=_key;
-            val=_val;
-        }
-};
 class LRUCache {
 public:
-    Node* head=new Node(-1,-1);
-    Node* tail=new Node(-1,-1);
+    class Node{
+        public:
+        int key;
+        Node* next;
+        Node* prev;
+        int val;
+
+        Node(int key_,int val_){
+            key=key_;
+            val=val_;
+        }
+    };
+
     int cap;
     unordered_map<int,Node*> mpp;
+    Node* head=new Node(-1,-1);
+    Node* tail=new Node(-1,-1);
 
-    LRUCache(int capacity) {
-        head->next=tail;
-        tail->prev=head;
-        cap=capacity;
+    void deletenode(Node* node){
+        node->next->prev=node->prev;
+        node->prev->next=node->next;
     }
 
     void addnode(Node* node){
-         node->next = head->next;
-    node->prev = head;
-    head->next->prev = node;
-    head->next = node;
+        node->next=head->next;
+        node->prev=head;
+        head->next->prev=node;
+        head->next=node;
     }
 
-    void deletenode(Node* node){
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
-        //delete(node);
+    LRUCache(int capacity) {
+        cap=capacity;
+         head->next = tail;
+        tail->prev = head;
     }
     
     int get(int key) {
         if(mpp.find(key) != mpp.end()){
-            Node* node= mpp[key];
-            int val=node->val;
+            Node* node=mpp[key];
             deletenode(node);
             addnode(node);
-            return val;
+            return node->val;
         }
-        return -1;
+        else{
+            return -1;
+        }
     }
     
     void put(int key, int value) {
         if(mpp.find(key) != mpp.end()){
-            Node* node= mpp[key];
-            node->val=value;
+            Node* node=mpp[key];
             deletenode(node);
+            node->val=value;
             addnode(node);
             return;
         }
-
+        
         if(mpp.size()==cap){
-            Node* lru=tail->prev;
-            mpp.erase(lru->key);
-            deletenode(lru);
-            delete(lru);
+            mpp.erase(tail->prev->key);
+            deletenode(tail->prev);
         }
+
         Node* newnode=new Node(key,value);
         addnode(newnode);
         mpp[key]=newnode;
