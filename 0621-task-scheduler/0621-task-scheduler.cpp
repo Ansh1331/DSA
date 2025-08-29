979db2a3b0e22a501a56048f8c1ba428897a10a2
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-08-30 01:55:41 +0530

    Time: 34 ms (42.46%), Space: 47.7 MB (19.68%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& arr, int n) {
-        // int n=arr.size();
-        priority_queue<int> pq;
-        vector<int> mpp(26,0);
-        for(int i=0;i<arr.size();i++){
-            mpp[arr[i]-'A']++;
-        }
-        int res=0;
 
-        for(int i=0;i<26;i++){
-            if(mpp[i]>0){
-                pq.push(mpp[i]);
-            }
+    int leastInterval(vector<char>& arr, int n) {
+        priority_queue<int> pq;
+        unordered_map<char,int>mpp;
+        for(auto it:arr){
+            mpp[it]++;
+        }
+        for(auto it:mpp){
+            pq.push(it.second);
         }
 
         int time=0;
         while(!pq.empty()){
-            vector<int>temp;
-            for(int i=0;i<=n;i++){
+            vector<int> temp;
+            for(int i=1;i<=n+1;i++){
                 if(!pq.empty()){
                     int freq=pq.top();
                     pq.pop();
@@ -26,11 +22,14 @@ public:
                     temp.push_back(freq);
                 }
             }
-            for(int i=0;i<temp.size();i++){
-                if(temp[i]>0){
-                    pq.push(temp[i]);
-                }
-            }
+
+            for (int i = 0; i < temp.size(); i++) {
+    if (temp[i] > 0) {   // ✅ only push tasks that are still left
+        pq.push(temp[i]);
+    }
+}
+
+
             if(pq.empty()){
                 time+=temp.size();
             }
