2ea65635a17c7da5252bda4f6b1ba43566ae565b
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-01 02:08:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    int helper(int n,vector<int>& dp,vector<int>& arr){
-        if(n==0 || n<0) return arr[0];
-        if(dp[n]!=-1) return dp[n];
-        
-        //rob
-        int pick= arr[n];
-        if(n>1){
-            pick= arr[n] + helper(n-2,dp,arr);
+     int helper(vector<int>&dp, int i,vector<int>&arr){
+        if(i==0){
+            return arr[0]; 
         }
-        //skip
-        int skip= helper(n-1,dp,arr);
-        
-        return dp[n]=max(pick,skip);
+
+        if(dp[i]!=-1) return dp[i];
+
+        int nottake=helper(dp,i-1,arr);
+        int take=0;
+        if(i>1){
+            take= arr[i]+helper(dp,i-2,arr);
+        }
+        return dp[i]=max(take,nottake);
     }
 
     int rob(vector<int>& arr) {
         int n=arr.size();
         vector<int> dp(n+1,-1);
-        // return helper(n-1,dp,arr);
-        dp[0]= arr[0];
-
-        for(int i=1;i<n;i++){
-            int pick= arr[i];
-            if(i>1){
-                pick= arr[i] + dp[i-2];
-            }
-            //skip
-            int skip=dp[i-1];
-            
-            dp[i]=max(pick,skip);
-        }
-        return dp[n-1];
+        return helper(dp,n-1,arr);
     }
 };
