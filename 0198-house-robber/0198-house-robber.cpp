701b8ac2909cc0eb12394c7d6f284a68b4c9a69a
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-01 02:08:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
      int helper(vector<int>&dp, int i,vector<int>&arr){
         if(i==0){
-            return arr[0]; 
+            return dp[0]; 
         }
 
         if(dp[i]!=-1) return dp[i];
 
         int nottake=helper(dp,i-1,arr);
-        int take=0;
+        int take=arr[i];
         if(i>1){
             take= arr[i]+helper(dp,i-2,arr);
         }
@@ -18,6 +18,7 @@ public:
     int rob(vector<int>& arr) {
         int n=arr.size();
         vector<int> dp(n+1,-1);
+        dp[0]=arr[0];
         return helper(dp,n-1,arr);
     }
 };
