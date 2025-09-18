d27f9d66092c01c48f93b5b3e0025c3a8c99518e
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-19 00:28:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,29 +3,16 @@ public:
     int subarraySum(vector<int>& arr, int k) {
         unordered_map<int,int> mpp;
         mpp[0]=1;
-
-        int sum=0;
         int count=0;
+        int sum=0;
         for(int i=0;i<arr.size();i++){
-            sum+= arr[i];
-            int rem= sum-k;
-            if(mpp.find(rem) != mpp.end()){
-                count+= mpp[rem];
+            sum+=arr[i];
+            int rem=abs(k-sum);
+            if(mpp.find(rem)!=mpp.end()){
+                count++;
             }
             mpp[sum]++;
-        }   
+        }
         return count;
     }
 };
-
-
-// 1 2 3
-/*
-
-0 - 0
-1 - 1
-
-
-
-
-*/
