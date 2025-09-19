19c5aa8b3f381d6432bf27b1fe97eee40256c1ba
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-19 15:37:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,25 +4,15 @@ public:
         int n=s.size();
         int m=t.size();
         if(n!=m) return false;
-        vector<int> stringS(200,0);
-        vector<int> stringT(200,0);
+        vector<int> mpp1(200,0);
+        vector<int> mpp2(200,0);
 
         for(int i=0;i<n;i++){
-            if(stringS[s[i]] != stringT[t[i]]) return false;
-            stringS[s[i]] =i+1;
-            stringT[t[i]] =i+1;
+            if(mpp1[s[i]]!= mpp2[t[i]]) return false;
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
         }
+
         return true;
-
-
     }
 };
-
-/*
-f o o
-b a r
-
-
-
-
-*/
