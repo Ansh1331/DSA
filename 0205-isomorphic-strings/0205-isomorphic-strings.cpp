bdfb597ef506c43ebe7dbb5f583a0f081af1bf78
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-19 15:37:17 +0530

    Time: 0 ms (100%), Space: 9.2 MB (80.49%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -9,8 +9,8 @@ public:
 
         for(int i=0;i<n;i++){
             if(mpp1[s[i]]!= mpp2[t[i]]) return false;
-            mpp1[s[i]]++;
-            mpp2[t[i]]++;
+            mpp1[s[i]]=i+1;
+            mpp2[t[i]]=i+1;
         }
 
         return true;
