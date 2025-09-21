a0644a478a02bedd9013a19ee1ba2ea3546b707e
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-21 16:51:58 +0530

    Time: 78 ms (10.69%), Space: 72.1 MB (8.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    void helper(vector<int>&arr, vector<vector<int>>&res,vector<int>temp,int index,int n,int sum,int target){
-        if(index==n){
-            if(sum==target){
-                res.push_back(temp);
-                return;
-            }
+    void helper(vector<int>& arr,vector<vector<int>>& res,vector<int> temp,int index,int n,int sum,int currentsum){
+        if(currentsum==sum){
+            res.push_back(temp);
             return;
         }
 
-        //exclude
-        helper(arr,res,temp,index+1,n,sum,target);
-
-        if(sum+arr[index] <= target){
-            temp.push_back(arr[index]);
-            helper(arr,res,temp,index,n,sum+arr[index],target);
+        if(currentsum>sum || index>=n){
+            return;
         }
-        
+
+        helper(arr,res,temp,index+1,n,sum,currentsum);
+
+        temp.push_back(arr[index]);
+        helper(arr,res,temp,index,n,sum,currentsum+arr[index]);
+        temp.pop_back();
     }
 
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        int n=arr.size();
         vector<vector<int>> res;
         vector<int> temp;
-        int n=arr.size();
-        int sum=0;
-
-        helper(arr,res,temp,0,n,sum,target);
+        int currentsum=0;
+        helper(arr,res,temp,0,n,target,currentsum);
         return res;
     }
 };
