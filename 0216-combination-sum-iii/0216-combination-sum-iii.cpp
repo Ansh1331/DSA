8a0a799877cf791912b5ebb5f45533d2ed81ca10
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-21 17:07:19 +0530

    Time: 2 ms (23.71%), Space: 9.4 MB (22.64%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>&res,vector<int>temp,int index,int sum,int target,int k){
-        if(temp.size()==k && sum==target){
+    void helper(vector<vector<int>>& res,vector<int> temp,int index,int sum,int currentsum,int steps,int k){
+        if(currentsum==sum && steps==k){
             res.push_back(temp);
             return;
         }
 
-        if(temp.size()> k || sum>target){
-            return;
-        }
-
-        for(int i=index;i<10;i++){
+        for(int i=index;i<=9;i++){
+            if(i>index && i==i-1) continue;
+            if(currentsum + i >sum || steps>k) return;
             temp.push_back(i);
-            helper(res,temp,i+1,sum+i,target,k);
+            helper(res,temp,i+1,sum,currentsum+i,steps+1,k);
             temp.pop_back();
+
         }
     }
 
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> res;
-        //sort(arr.begin(),arr.end());
         vector<int> temp;
-        //int n=arr.size();
-        int sum=0;
-        helper(res,temp,1,sum,n,k);
+        int currentsum=0;
+        int steps=0;
+        helper(res,temp,1,n,currentsum,steps,k);
         return res;
     }
 };
