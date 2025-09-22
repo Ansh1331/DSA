ef220bfa23e61aab4834fb46c8c49e4938c39518
Ansh Singhal <145444859+Ansh1331@users.noreply.github.com>
2025-09-22 16:05:01 +0530

    Time: 3 ms (42.11%), Space: 12.9 MB (15.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> nextGreaterElement(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int,int> mpp;
         stack<int> st;
-        unordered_map <int,int> mpp;
+        int n=arr2.size();
 
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty() && nums2[i]>=st.top()){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr2[i]>=st.top()){
                 st.pop();
             }
-            mpp[nums2[i]]= st.empty() ? -1:st.top();
-            st.push(nums2[i]);
+
+            mpp[arr2[i]]= st.empty()? -1:st.top();
+            st.push(arr2[i]);
         }
-        vector<int> res(nums1.size());
-        for(int i=0;i<nums1.size();i++){
-            res[i]=mpp[nums1[i]];
+
+        vector<int> res(arr1.size(),-1);
+        for(int i=0;i<arr1.size();i++){
+            res[i]=mpp[arr1[i]];
         }
         return res;
     }
